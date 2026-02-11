a5bcc4f4c6aca2f06188a375738c57c24b2b451e
bazarsad <bazarsadadiya8@gmail.com>
2026-02-11 13:17:10 +0800

    lab2 hiisen

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
+        // 1. Rectangle үүсгэх
+        Shape rectangle = new Rectangle(2, 3);
         Renderer renderer = new Renderer(rectangle);
+        renderer.draw();
 
+        // 2. Circle үүсгэх (Renderer-ийг өөрчлөх шаардлагагүй!)
+        Shape circle = new Circle(5);
+        renderer = new Renderer(circle); 
+        renderer.draw();
+
+        // 3. Square үүсгэх
+        Shape square = new Square(4);
+        renderer = new Renderer(square);
         renderer.draw();
     }
 }
